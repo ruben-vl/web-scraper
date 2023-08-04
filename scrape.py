d7c81d0361c3d3b4eda7a81a7a427a0c2b923542
Ruben Van Laer <rubenvanlaer@mail.com>
2023-08-04 17:41:44 +0200

    Create dataframe with all data

--- scrape.py
+++ scrape.py
@@ -1,19 +1,15 @@
 from cookies import COOKIE_NAME, COOKIE_VALUE
 
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 URL = "https://icts.kuleuven.be/masterproeven/student/targetgroups/1158"
-
 cookies = {COOKIE_NAME: COOKIE_VALUE}
-
 page = requests.get(URL, cookies=cookies)
+parsed_page = BeautifulSoup(page.content, "html.parser")
 
-soup = BeautifulSoup(page.content, "html.parser")
-
-topic_table = soup.find(id="selectsubject")
-
-topics = topic_table.find_all(class_="subject-card")
+topics = parsed_page.find_all(class_="subject-card")
 
 topic_info = []
 
@@ -33,5 +29,31 @@ for topic in topics:
     result.append(detail_url["href"])
     topic_info.append(result)
 
-print(topic_info)
+data = {
+    'Title': [],
+    'Promotor': [],
+    'Co-promotor': [],
+    'Target groups': [],
+    'Disciplines': [],
+    '#students': [],
+    'link': []
+}
 
+for row in topic_info:
+    data['Title'].append(row[0])
+    data['Promotor'].append(row[1])
+    if len(row) == 6:
+        data['Co-promotor'].append("")
+        data['Target groups'].append(row[2])
+        data['Disciplines'].append(row[3])
+        data['#students'].append(row[4])
+        data['link'].append(row[5])
+    elif len(row) == 7:
+        data['Co-promotor'].append(row[2])
+        data['Target groups'].append(row[3])
+        data['Disciplines'].append(row[4])
+        data['#students'].append(row[5])
+        data['link'].append(row[6])
+
+df = pd.DataFrame(data)
+print(df)
